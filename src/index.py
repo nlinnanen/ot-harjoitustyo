e5109a9de29f6ed4eb999e7b11a59c1ca5c1227a
Niklas Linnanen <nlinnanen@gmail.com>
2024-04-03 21:44:13 +0300

    Add basic commands

--- src/index.py
+++ src/index.py
@@ -1,5 +1,5 @@
 
-from repositories import user_repository
+from repositories import member_repository, user_repository
 from services import user_service
 
 
@@ -13,6 +13,53 @@ def main():
             break
         else:
             print("Invalid username or password")
+    
+    print("You are now logged in")
+
+    while True:
+        command = input("Enter a command: ")
+        match command.lower():
+            case "exit":
+                print("Goodbye")
+                break
+            case "help":
+                print("Help message")
+            case "list members":
+                print("List of members:")
+                members = member_repository.get_all_members()
+                for member in members:
+                    print(member)
+            case "update member":
+                member = None
+                while True:
+                    id = input("Enter member ID: ")
+                    member = member_repository.get_member_by_id(id)
+                    if not member:
+                        print("Invalid member ID")
+                    else:
+                        break
+                    
+                print(f"Member {member}")
+                
+                while True:
+                    field = input("Enter field to update (empty to exit): ")
+                    if field == "":
+                        break
+
+                    if field not in member.__dict__:
+                        print("Invalid field")
+                        continue
+
+                    value = input("Enter new value: ")
+                    member_repository.update_member(member.id, **{field: value})
+                    print("Member updated")
+                    
+                
+
+            case "delete member":
+                print("Delete message")
+            case _:
+                print("Unknown command")
     
 
 if __name__ == "__main__":
