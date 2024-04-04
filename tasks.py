88657f88951237dbb78da69d309c23dc030a83c1
Niklas Linnanen <nlinnanen@gmail.com>
2024-04-04 18:23:46 +0300

    Add test db and pytest library

--- tasks.py
+++ tasks.py
@@ -6,5 +6,17 @@ def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
 
 @task
+def test(ctx):
+    ctx.run("python3 -m unittest discover -s src/tests", pty=True)
+
+@task
 def create_sample_data(ctx):
     ctx.run("python3 src/create_sample_data.py", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
